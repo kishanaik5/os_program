4464396ce86221f1ec85aa977ed1d667bcf9ac80
Kishan K <120300882+kishanaik5@users.noreply.github.com>
2024-01-07 19:34:08 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<dirent.h>
+struct direct *dptr;
+
+int main(int args, char *argv[]){
+    char buff[100];
+    DIR *dirp;
+    printf("Enter the directory name\n");
+    scanf("%s",buff);
+    if (((dirp=opendir(buff))==NULL){
+        printf("The directory name doesn't exist\n");
+        exit(1);
+    }
+    while(dptr=readdir(dirp)){
+        printf("%s\n",dptr->d_name);
+    }
+    closedir(dirp);
+}
